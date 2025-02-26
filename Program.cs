edc524a4810413335ea0506771c712e2755da676
Furkan Emre Saygin <furkanemresaygin@gmail.com>
2025-02-26 16:49:05 +0300

    Koşul Yapıları (if-else ve switch-case)

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        //-------------------Negatif Pozitif Sayı-------------------//
+        /*
+        Console.WriteLine("Enter a number: ");
+        int Number = Convert.ToInt32(Console.ReadLine());
+
+        if (Number > 0) { 
+            Console.WriteLine("The number is positive.");
+        } else if (Number < 0) {
+            Console.WriteLine("The number is negative.");
+        } else {
+            Console.WriteLine("The number is zero.");
+        }
+        */
+
+        //-------------------1 ile 100 arasındaki sadece çift sayıları ekrana yazdırma-------------------//
+        /*
+        for (int i = 1; i <= 100; i++) {
+            
+            if (i % 2 == 0 && i % 10 != 0) { // 2 ye tam bölünen ve 10 a tam bölünmeyen sayıları yazdırır.
+                Console.WriteLine(i);
+            }
+            
+            if (i == 50) { // 50 olduğunda döngü durur.
+                break;
+            }
+        }
+        */
+        // --------------------- Foreach Kullanımı---------------------// 
+
+        string[] names = { "Ali", "Ayşe", "Mehmet", "Zeynep", "Hasan" }; 
+
+        foreach (string isim in names) { 
+
+            if (isim == "Zeynep") 
+
+            { 
+
+                continue; // "Zeynep" ismini atlayarak döngüye devam eder 
+
+            } 
+
+            Console.WriteLine(isim); // if döngüsüne takılmayan isimleri yazdırır. 
+
+        } 
+
+    }
+}
